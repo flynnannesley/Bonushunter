2d8da95a5109fa5de3e11b3e21b648fc4431d717
Flynn Annesley <flynnannesley@Flynns-MacBook-Air.local>
2025-08-01 15:52:25 +0200

    Initial commit

---
+++ AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div className="p-6 text-center">
+      <h2 className="text-4xl font-bold mb-4">About the bonushunter</h2>
+      <p>The bonushunter gathers all of the top online casinos and gambing sites in South Africa in 2025. We update, inform and recommend the best sites nationwide for the ultimate user experience. The bonushunter was started and established in Cape Town in 2025 and is run independently. If you are wishing to contact the bonushunter, please do not hesitate to refer to the support page for more details</p>
+    </div>
+  );
+}
+
+export default AboutPage;
